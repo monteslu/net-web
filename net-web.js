be6f7c0a142d2212371458fa4120936f96279acd
Luis Montes <monteslu@gmail.com>
2023-02-02 16:27:40 -0700

    init

---
+++ net-web.js
@@ -0,0 +1,69 @@
+const EventEmitter = require('events').EventEmitter;
+
+const events = new EventEmitter();
+
+function Socket() {
+  const socket = new EventEmitter();
+  
+  socket.connect = function(port, host, clientCallback) {
+    events.emit(('socket_connect_' + port), {
+      port,
+      host,
+      clientSocket: socket,
+      clientCallback,
+    });  
+  }
+
+  socket.write = function(message) {
+    if (socket.serverSocket) {
+      socket.serverSocket.emit('data', message);
+    }
+  };
+
+  socket.end = function() {
+    if (socket.serverSocket) {
+      socket.serverSocket.emit('close');
+    }
+    
+  }
+
+  return socket;
+}
+
+function createServer(cb) {
+  const server = new EventEmitter();
+  server.listen = (port) => {
+    console.log('server.listen', port);
+    listeners['l' + port] = server;
+    events.on('socket_connect_' + port, ({ clientSocket, clientCallback }) => {
+      console.log('socket_connect_' + port, clientSocket);
+      const serverSocket = new EventEmitter();
+      clientSocket.serverSocket = serverSocket;
+      if (server.cb) {
+        server.cb(serverSocket);
+      }
+      serverSocket.write = (data) => {
+        clientSocket.emit('data', data);
+      }
+      serverSocket.end = () => {
+        clientSocket.emit('close');
+      }
+      
+      if (clientCallback) {
+        clientCallback();
+      }
+    });
+  };
+  server.cb = cb;
+  return server;
+}
+
+// hacky, but I need all things using 'net' to have the same listeners
+const net = global.nodeNetWeb || {
+  Socket,
+  createServer,
+  events,
+  EventEmitter,
+};
+
+module.exports = net;
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const mode = process.env.BUILD_MODE || 'development';
+
+module.exports = {
+  entry: {
+    "j5": "./net-web.js",
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: mode === 'development' ? 'net-web.js' : 'net-web.min.js'
+  },
+  mode,
+  resolve: {
+    alias: {
+    }
+  },
+};
